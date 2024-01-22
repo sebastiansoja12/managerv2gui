b297d181635bc59fd251aa85d84ef94cf05f25cb
Sebastian Soja <sebastian5253@wp.pl>
2024-01-22 19:00:51 +0100

    FFT-445 Change layout, fix bug while updating property

--- src/components/SoftwareConfiguration/SoftwareConfiguration.tsx
+++ src/components/SoftwareConfiguration/SoftwareConfiguration.tsx
@@ -1,86 +1,38 @@
-import React, {useState} from 'react';
-import {
-    Button,
-    createTheme, makeStyles,
-    Paper,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
-    TextField,
-    ThemeProvider
-} from '@mui/material';
-import Software from "./model/Software";
-
-
-const useStyles = makeStyles({
-    tableContainer: {
-        maxWidth: '80%', // Set the maximum width of the table container
-        margin: 'auto', // Center the table
-    },
-    table: {
-        minWidth: 400, // Set the minimum width of the table
-    },
-    button: {
-        textTransform: 'none',
-    },
-});
+import React, { useState } from 'react';
+import { Button, createTheme, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, ThemeProvider } from '@mui/material';
+import Software from './model/Software';
 
 interface SoftwarePropertiesListProps {
     softwareProperties: Software[];
     onUpdate: (id: string, category: string, name: string, value: string) => void;
 }
 
-
-
-const SoftwarePropertiesList: React.FC<SoftwarePropertiesListProps> = ({softwareProperties, onUpdate}) => {
-    const [editableFields, setEditableFields] = useState<{
-        [key: string]: { name: string, category: string, value: string }
-    }>({});
+const SoftwarePropertiesList: React.FC<SoftwarePropertiesListProps> = ({ softwareProperties, onUpdate }) => {
+    const [editableFields, setEditableFields] = useState<{ [key: string]: { name: string, category: string, value: string } }>({});
 
     const handleFieldChange = (id: string, field: string, value: string) => {
         setEditableFields((prevFields) => ({
             ...prevFields,
             [id]: {
                 ...prevFields[id],
-                [field]: value
-            }
+                [field]: value,
+            },
         }));
     };
 
     const handleUpdateClick = (property: Software) => {
-        const {id, category, name} = property;
+        const { id, category, name } = property;
         const updatedFields = editableFields[id] || {};
 
         onUpdate(id, updatedFields.category || category, updatedFields.name || name, updatedFields.value || property.value);
-        setEditableFields((prevFields) => ({...prevFields, [id]: {} as any}));
+        setEditableFields((prevFields) => ({ ...prevFields, [id]: {} as any }));
     };
 
-    const theme = createTheme({
-        components: {
-            MuiTableCell: {
-                styleOverrides: {
-                    root: {
-                        borderBottom: '1px solid rgba(224, 224, 224, 1)',
-                        padding: '8px', // Adjust cell padding
-                    },
-                },
-            },
-            MuiButton: {
-                styleOverrides: {
-                    root: {
-                        textTransform: 'none',
-                    },
-                },
-            },
-        },
-    });
+    const theme = createTheme();
 
     return (
         <ThemeProvider theme={theme}>
-            <TableContainer component={Paper} >
+            <TableContainer component={Paper} sx={{ maxWidth: '80%', margin: 'auto' }}>
                 <Table>
                     <TableHead>
                         <TableRow>
@@ -116,7 +68,7 @@ const SoftwarePropertiesList: React.FC<SoftwarePropertiesListProps> = ({software
                                 <TableCell>
                                     <Button
                                         variant="contained"
-                                        color="primary"
+                                        color="info"
                                         onClick={() => handleUpdateClick(property)}
                                     >
                                         Update
