3d2c8d22c9c866c2227de5b66268d363042a218c
Sebastian Soja <sebastian5152@wp.pl>
2026-07-14 18:34:09 +0200

    feat(departments): add department creation request

--- src/hooks/DepartmentService.tsx
+++ src/hooks/DepartmentService.tsx
@@ -1,5 +1,23 @@
 import Department from "../class/depots/Department";
 import http from "../http-common";
+
+export type DepartmentCreateRequest = {
+    departments: Array<{
+        departmentCode: {
+            value: string;
+        };
+        city: string;
+        street: string;
+        postalCode: string;
+        nip: string;
+        telephoneNumber: string;
+        openingHours: string;
+        email: string;
+        countryCode: string;
+        departmentType: string;
+    }>;
+};
+
 const getAll = () => {
     return http.get<Array<Department>>("/departments");
 };
@@ -7,7 +25,7 @@ const get = (depotCode: any) => {
     return http.get<Department>(`/depots/${depotCode}`);
 };
 
-const create = (data: Department) => {
+const create = (data: DepartmentCreateRequest) => {
     return http.post<Department>("/departments", data);
 };
 
