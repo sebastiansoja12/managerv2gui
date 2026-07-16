7ad8a6f04ccfd70fb91f2b5f20f528e0cadcce9d
Sebastian Soja <sebastian5152@wp.pl>
2026-07-16 19:39:59 +0200

    Updates HTTP client http-software-common

--- src/http-software-common.ts
+++ src/http-software-common.ts
@@ -1,20 +1,12 @@
 import axios from "axios";
-import {getAuthToken} from "./auth/AuthTokenStorage";
+import {configureAuthenticatedClient} from "./auth/configureAuthenticatedClient";
 
 const http = axios.create({
     baseURL: `${process.env.SOFTWARE_APP_SERVER_URL}`,
     headers: {
         "Content-type": "application/json"
-    }
+    },
+    withCredentials: true,
 });
 
-http.interceptors.request.use((config) => {
-    const token = getAuthToken();
-    if (token) {
-        config.headers.set("Authorization", `Bearer ${token}`);
-    }
-
-    return config;
-});
-
-export default http;
+export default configureAuthenticatedClient(http);
