42b86e380b0c3fbec1537f0be9761b01502d17b2
Sebastian Soja <sebastian5152@wp.pl>
2026-07-16 19:39:58 +0200

    Updates interface component App

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
+import { ThemeProvider } from '@mui/material';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
 import AppShell from "./components/AppShell/AppShell";
 import SuperAdminApplication from "./components/SuperAdmin/SuperAdminApplication";
+import appTheme from "./theme/appTheme";
 
 function App() {
     return (
-        <div className="App">
-            <div className="page-container">
-                <div className="content-wrapper">
-                    <BrowserRouter>
-                        <Routes>
-                            <Route path="/super-admin/*" element={<SuperAdminApplication/>}/>
-                            <Route path="/*" element={<AppShell/>}/>
-                        </Routes>
-                    </BrowserRouter>
+        <ThemeProvider theme={appTheme}>
+            <div className="App">
+                <div className="page-container">
+                    <div className="content-wrapper">
+                        <BrowserRouter>
+                            <Routes>
+                                <Route path="/super-admin/*" element={<SuperAdminApplication/>}/>
+                                <Route path="/*" element={<AppShell/>}/>
+                            </Routes>
+                        </BrowserRouter>
+                    </div>
                 </div>
             </div>
-        </div>
+        </ThemeProvider>
     );
 }
 
