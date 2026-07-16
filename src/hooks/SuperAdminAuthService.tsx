54e155c0e8ebc095419d7c8f03752489f53899c4
Sebastian Soja <sebastian5152@wp.pl>
2026-07-16 19:39:59 +0200

    Updates authentication mechanism SuperAdminAuthService

--- src/hooks/SuperAdminAuthService.tsx
+++ src/hooks/SuperAdminAuthService.tsx
@@ -1,9 +1,8 @@
 import http from "../http-super-admin";
-import {AuthenticationToken} from "../components/LoginPage/model/AuthenticationToken";
 import {LoginRequest} from "../components/LoginPage/model/LoginRequest";
 
 const login = (loginRequest: LoginRequest) => {
-    return http.post<AuthenticationToken>("/auth/login", loginRequest);
+    return http.post<void>("/auth/login", loginRequest);
 };
 
 const SuperAdminAuthService = {
